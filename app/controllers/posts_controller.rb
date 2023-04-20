d72f23774b471531c8e0c8cd3f80ec6917fca2aa
Kenta Kato <01keny01@gmail.com>
2023-04-20 19:43:54 +0900

    add: create post fucntion

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ActionController::Base
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
